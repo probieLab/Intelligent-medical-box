9b5c66e3fd59d7f6b4d7b6d8ce0b7f024886b918
Tommy8081 <1247527652@qq.com>
2018-06-02 22:09:32 +0800

    1

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 export default class App extends Component {
-  render() {
+  render()  {
     return (
       <TotalNav/>
     )
